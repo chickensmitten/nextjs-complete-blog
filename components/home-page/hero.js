fe8254e5ec0b157eb6e75e8c4057920609f0f668
sheng yeong <chia.shengyeong@gmail.com>
2022-08-29 10:56:53 +0800

    added hero and npm installed

---
+++ components/home-page/hero.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import classes from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div>
+        <Image src="/images/site/max.png" alt="Owner's image" width={640} height={800}/>
+      </div>
+      <h1>Hi, Im Max</h1>
+      <p>Lorem ipsum text</p>
+    </section>
+  );
+}
+
+export default Hero;
