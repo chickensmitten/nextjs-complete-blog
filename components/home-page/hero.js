ba54669b422a8fdf1119cdfe978de78f35bdc4b2
sheng yeong <chia.shengyeong@gmail.com>
2022-08-29 11:13:24 +0800

    added layout and navigation

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -4,7 +4,7 @@ import classes from './hero.module.css';
 function Hero() {
   return (
     <section className={classes.hero}>
-      <div>
+      <div className={classes.image}>
         <Image src="/images/site/max.png" alt="Owner's image" width={320} height={400}/>
       </div>
       <h1>Hi, Im Max</h1>
---
+++ components/layout/logo.js
@@ -0,0 +1,11 @@
+import classes from "./logo.module.css";
+
+function Logo() {
+  return (
+    <div className={classes.logo}>
+      Max blog
+    </div>
+  );
+}
+
+export default Logo;
