803b6c4efc5f78507f791625753c77ff9571a10a
sheng yeong <chia.shengyeong@gmail.com>
2022-08-29 10:57:58 +0800

    minor updates

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className={classes.hero}>
       <div>
-        <Image src="/images/site/max.png" alt="Owner's image" width={640} height={800}/>
+        <Image src="/images/site/max.png" alt="Owner's image" width={320} height={400}/>
       </div>
       <h1>Hi, Im Max</h1>
       <p>Lorem ipsum text</p>
